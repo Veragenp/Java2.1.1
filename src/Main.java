1a48094ff8f12afde1eb7e96fdf314e060c457f7
Yury <yp@unotex.ru>
2021-07-26 14:02:45 +0700

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int ticket_price = 150;
+
+        int mile_price = 20;
+        int miles = ticket_price / mile_price;
+
+        System.out.println("Мили за билет - " + miles);
+    }
+}
