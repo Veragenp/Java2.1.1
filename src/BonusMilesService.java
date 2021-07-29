e5da117b136323dbeaca65562911886bda3a15eb
Yury <yp@unotex.ru>
2021-07-29 12:26:37 +0700

    Added BonusmilesSerice

---
+++ src/BonusMilesService.java
@@ -0,0 +1,7 @@
+public class BonusMilesService {
+    public int calculate (int price) {
+        int milePrice = 20;
+        int miles = price / milePrice;
+        return miles;
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-
-        int ticketPrice = 150;
-        int milePrice = 20;
-        int miles = ticketPrice / milePrice;
-        System.out.println("Мили за билет - " + miles);
+        BonusMilesService service = new BonusMilesService();
+        int price = 5000;
+        int miles = service.calculate(price);
+        System.out.println(miles);
     }
 }
